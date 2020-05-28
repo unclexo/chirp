3703d90e64b7dc89d086780f5f1b590d3cb7cf01
Benjamin Crozat <benjamincrozat@me.com>
2020-05-28 18:02:32 +0200

    Now rendering hashtags too

--- app/Presenters/TweetPresenter.php
+++ app/Presenters/TweetPresenter.php
@@ -25,23 +25,8 @@ class TweetPresenter
 
     public function text() : string
     {
-        $text = $this->tweet->full_text;
-
-        $stringsToReplace    = [];
-        $stringsReplacements = [];
-
-        foreach ($this->tweet->entities->user_mentions as $mention) {
-            $stringsToReplace[]    = mb_substr($text, $mention->indices[0], $mention->indices[1] - $mention->indices[0]);
-            $stringsReplacements[] = '<a href="https://twitter.com/' . $mention->screen_name . '" target="_blank" rel="noopener" class="font-semibold hover:text-white">@' . $mention->screen_name . '</a>';
-        }
-
-        foreach ($stringsToReplace as $key => $stringToReplace) {
-            $text = mb_ereg_replace($stringToReplace, $stringsReplacements[$key], $text);
-        }
-
-        foreach ($this->tweet->entities->urls as $url) {
-            $text = str_replace($url->url, '<a href="' . $url->url . '" target="_blank" rel="noopener" class="font-semibold hover:text-white">' . $url->display_url . '</a>', $text);
-        }
+        $text = $this->renderHashtagsAndMentions($this->tweet->full_text);
+        $text = $this->renderLinks($text);
 
         return nl2br($text);
     }
@@ -59,5 +44,36 @@ class TweetPresenter
     public function __get($key)
     {
         return $this->tweet->$key;
+    }
+
+    protected function renderHashtagsAndMentions(string $text) : string
+    {
+        $stringsToReplace    = [];
+        $stringsReplacements = [];
+
+        foreach ($this->tweet->entities->user_mentions as $mention) {
+            $stringsToReplace[]    = mb_substr($text, $mention->indices[0], $mention->indices[1] - $mention->indices[0]);
+            $stringsReplacements[] = '<a href="https://twitter.com/' . $mention->screen_name . '" target="_blank" rel="noopener" class="font-semibold hover:text-white">@' . $mention->screen_name . '</a>';
+        }
+
+        foreach ($this->tweet->entities->hashtags as $hashtag) {
+            $stringsToReplace[]    = mb_substr($text, $hashtag->indices[0], $hashtag->indices[1] - $hashtag->indices[0]);
+            $stringsReplacements[] = '<a href="https://twitter.com/hashtag/' . $hashtag->text . '" target="_blank" rel="noopener" class="font-semibold hover:text-white">#' . $hashtag->text . '</a>';
+        }
+
+        foreach ($stringsToReplace as $key => $stringToReplace) {
+            $text = mb_ereg_replace($stringToReplace, $stringsReplacements[$key], $text);
+        }
+
+        return $text;
+    }
+
+    protected function renderLinks(string $text) : string
+    {
+        foreach ($this->tweet->entities->urls as $url) {
+            $text = str_replace($url->url, '<a href="' . $url->url . '" target="_blank" rel="noopener" class="font-semibold hover:text-white">' . $url->display_url . '</a>', $text);
+        }
+
+        return $text;
     }
 }
