49d45e26f602f66b0932d8bd94ce3c6be79c23a1
Benjamin Crozat <benjamincrozat@me.com>
2020-05-29 10:08:05 +0200

    Text change

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,7 +9,7 @@
 
         <div class="sm:flex sm:items-center mt-4">
             <p>
-                <strong class="font-semibold">Everything will definitely be lost</strong>. You will still be able to come back to @config('app.name') whenever you want.
+                <strong class="font-semibold">Everything will definitely be lost</strong>, but you will still be able to come back to @config('app.name') whenever you want.
             </p>
 
             <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold inline-block md:ml-8 lg:ml-16 mt-4 sm:mt-0 mx-auto px-4 py-2 rounded text-red-100 table sm:inline-block hover:text-white">Supprimer</button>
