ab5fc9cbb69f0cd380fe30656dd146ae339def1a
Benjamin Crozat <benjamincrozat@me.com>
2020-05-29 09:58:29 +0200

    UI tweaks

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,16 +1,18 @@
 <x-layout>
-    <h2 class="font-semibold leading-none text-xl">Settings</h2>
+    <h3 class="font-semibold leading-none text-xl">Settings</h3>
 
-    <h3 class="font-semibold leading-none mt-8">Remove your account</h3>
-
-    <form method="POST" action="@route('user.delete')" class="sm:flex sm:items-center mt-4">
+    <form method="POST" action="@route('user.delete')" class="bg-gray-700 bg-opacity-50 mt-8 px-4 py-8 lg:p-8 rounded shadow">
         @csrf
         @method('DELETE')
 
-        <p>
-            <strong class="font-normal text-white">All your data will definitely be lost</strong>. You will still be able to come back to @config('app.name') whenever you want.
-        </p>
+        <h4 class="font-semibold leading-none">Remove your account</h4>
 
-        <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold inline-block sm:ml-8 mt-4 sm:mt-0 mx-auto px-4 py-2 rounded text-red-100 table sm:inline-block hover:text-white">Supprimer</button>
+        <div class="sm:flex sm:items-center mt-4">
+            <p>
+                <strong class="font-semibold">All your data will definitely be lost</strong>. You will still be able to come back to @config('app.name') whenever you want.
+            </p>
+
+            <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold inline-block md:ml-8 lg:ml-16 mt-4 sm:mt-0 mx-auto px-4 py-2 rounded text-red-100 table sm:inline-block hover:text-white">Supprimer</button>
+        </div>
     </form>
 </x-layout>
