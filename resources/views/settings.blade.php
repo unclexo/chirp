68c206744c9c4d81537800766443e855cd13f68e
Benjamin Crozat <benjamincrozat@me.com>
2020-05-29 10:02:57 +0200

    Text change

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -5,11 +5,11 @@
         @csrf
         @method('DELETE')
 
-        <h4 class="font-semibold leading-none">Remove your account</h4>
+        <h4 class="font-semibold leading-none">Remove your data</h4>
 
         <div class="sm:flex sm:items-center mt-4">
             <p>
-                <strong class="font-semibold">All your data will definitely be lost</strong>. You will still be able to come back to @config('app.name') whenever you want.
+                <strong class="font-semibold">Everything will definitely be lost</strong>. You will still be able to come back to @config('app.name') whenever you want.
             </p>
 
             <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold inline-block md:ml-8 lg:ml-16 mt-4 sm:mt-0 mx-auto px-4 py-2 rounded text-red-100 table sm:inline-block hover:text-white">Supprimer</button>
