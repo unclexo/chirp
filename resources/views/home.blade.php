2f57e6174707d28f2236442bee7a80fd603cbe83
Benjamin Crozat <benjamincrozat@me.com>
2020-05-30 14:16:56 +0200

    Whoops

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout :hide-sidebar="true">
     <div class="text-center">
-        <a href="@route('login')" class="bg-blue-500 hover:bg-blue-400 font-semibold inline-block px-8 py-4 rounded text-blue-100 hover:text-yellow-500">
+        <a href="@route('login')" class="bg-blue-500 hover:bg-blue-400 font-semibold inline-block px-8 py-4 rounded text-blue-100 hover:text-white">
             Sign in with Twitter
         </a>
     </div>
