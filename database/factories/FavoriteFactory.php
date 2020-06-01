4c47cc9324003017e28d324d40ba8b158c6278fe
Benjamin Crozat <benjamincrozat@me.com>
2020-06-01 10:33:12 +0200

    Improved favorites factory

--- database/factories/FavoriteFactory.php
+++ database/factories/FavoriteFactory.php
@@ -6,8 +6,15 @@ use Faker\Generator as Faker;
 /* @var \Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(Favorite::class, function (Faker $faker) {
+    $id       = $faker->randomNumber();
+    $favorite = json_decode(
+        file_get_contents(database_path('json/favorites-list.json'))
+    )[0];
+    $favorite->id     = $id;
+    $favorite->id_str = "$id";
+
     return [
-        'id'   => $faker->randomNumber(),
-        'data' => [],
+        'id'   => $id,
+        'data' => $favorite,
     ];
 });
