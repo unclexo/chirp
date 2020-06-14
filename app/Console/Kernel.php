02658ca9cfb2cb0f9d358dce7b1b6a23ff361e99
Benjamin Crozat <benjamincrozat@me.com>
2020-06-14 02:43:05 +0200

    Restored data fetching

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,13 @@
 
 namespace App\Console;
 
+use App\User;
+use App\Jobs\FetchUser;
+use App\Jobs\FetchLikes;
+use App\Jobs\FetchFollowers;
+use App\Jobs\FetchFollowings;
+use App\Jobs\FetchMutedUsers;
+use App\Jobs\FetchBlockedUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,6 +16,18 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule) : void
     {
+        // SQS doesn't support delayed jobs. We have to use the scheduler.
+
+        $schedule->call(function () {
+            User::cursor()->each(function (User $user) {
+                FetchBlockedUsers::dispatch($user);
+                FetchFollowers::dispatch($user);
+                FetchFollowings::dispatch($user);
+                FetchLikes::dispatch($user);
+                FetchMutedUsers::dispatch($user);
+                FetchUser::dispatch($user);
+            });
+        })->hourly();
     }
 
     protected function commands() : void
