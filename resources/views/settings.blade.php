62ad93dbd054b4e58f0b351c73dd68694540de8f
Benjamin Crozat <benjamincrozat@me.com>
2020-06-14 02:50:16 +0200

    Updated settings page

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,18 +1,20 @@
 <x-layouts.full>
     <h3 class="font-semibold leading-none text-center text-xl">Settings</h3>
 
-    <form method="POST" action="@route('user.delete')" class="bg-gray-700 bg-opacity-50 mt-8 px-4 py-8 lg:p-8 rounded shadow">
+    <form method="POST" action="@route('user.delete')" class="mt-8">
         @csrf
         @method('DELETE')
 
-        <h4 class="font-semibold leading-none">Remove your data</h4>
+        <h4 class="font-semibold leading-none text-lg">Remove your data</h4>
 
-        <div class="sm:flex sm:items-center mt-4">
-            <p>
+        <div class="mt-4">
+            <p class="leading-loose">
                 <strong class="font-semibold">Everything will definitely be lost</strong>, but you will still be able to come back to @config('app.name') whenever you want.
             </p>
 
-            <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold inline-block md:ml-8 lg:ml-16 mt-4 sm:mt-0 mx-auto px-4 py-2 rounded text-red-100 table sm:inline-block hover:text-yellow-500">Supprimer</button>
+            <p class="mt-4 text-center">
+                <button type="submit" class="bg-red-500 hover:bg-red-400 font-semibold px-4 py-2 rounded text-red-100 hover:text-yellow-500">Supprimer</button>
+            </p>
         </div>
     </form>
 </x-layouts.full>
