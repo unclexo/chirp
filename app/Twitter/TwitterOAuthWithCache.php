1fbffbabd79f56c158af9f236e625e4d0ba0e999
Benjamin Crozat <benjamincrozat@me.com>
2020-06-16 08:26:05 +0200

    Improved Twitter's API responses caching

--- app/Twitter/TwitterOAuthWithCache.php
+++ app/Twitter/TwitterOAuthWithCache.php
@@ -5,11 +5,12 @@ namespace App\Twitter;
 use Illuminate\Support\Facades\Cache;
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+// This is used for testing only.
 class TwitterOAuthWithCache extends TwitterOAuth
 {
     public function get($path, array $parameters = [])
     {
-        $key = md5($path . serialize($parameters));
+        $key = "{$path}_" . md5(serialize($parameters));
 
         if (Cache::has($key)) {
             $this->response->setHttpCode(200);
@@ -17,7 +18,12 @@ class TwitterOAuthWithCache extends TwitterOAuth
             return Cache::get($key);
         }
 
-        Cache::put($key, $response = parent::get($path, $parameters));
+        $response = parent::get($path, $parameters);
+
+        // If we get an error (likely "rate limit exceeded"), we don't cache the response.
+        if (empty($response->errors)) {
+            Cache::put($key, $response);
+        }
 
         return $response;
     }
